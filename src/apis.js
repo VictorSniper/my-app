067885bb1038b0a3764357b44c23bc2424e1cb16
sniper <sniper@sniperdeMacBook-Pro.local>
2021-09-12 09:50:34 +0800

    update

--- src/apis.js
+++ src/apis.js
@@ -7,5 +7,6 @@ export const getRedBlackList =params=>request('get','/community/redBlackList',pa
 export const getCityCategory =params=>request('get','/api/cityCategory',params)
 //删除
 export const districtDdelete =params=>request('post','/bDistrict/delete',params)
+
 export const getcustomer =params=>request('get','/api/kkk',params)
 
