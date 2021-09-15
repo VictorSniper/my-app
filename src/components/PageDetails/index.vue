9059f5e062932e0857a286a6de109c4be8ff0d58
chenxb <362197523@qq.com>
2021-09-15 15:46:10 +0800

    update

--- src/components/PageDetails/index.vue
+++ src/components/PageDetails/index.vue
@@ -1,7 +1,33 @@
 <template>
   <div class="page-content-details">
     <template v-for="(item, index) in config" :key="index">
-      <el-descriptions class="descriptions-margin" v-bind="item">
+      <el-descriptions
+        class="descriptions-margin"
+        @dragstart="dragstart($event, item, index)"
+        draggable="true"
+        @dragend="dragend"
+        v-bind="item"
+      >
+        <template v-for="(field, subIndex) in item.sub" :key="subIndex">
+          <el-descriptions-item>
+            <template #label>
+              {{ field.descriptionsLabel }}
+            </template>
+            {{ params[field.val] }}
+          </el-descriptions-item>
+        </template>
+      </el-descriptions>
+    </template>
+  </div>
+  <div @drop="drop" @dragover.prevent class="page-content-details-target">
+    <template v-for="(item, index) in targetObj" :key="index">
+      <el-descriptions
+        class="descriptions-margin"
+        @dragstart="dragstart($event, item)"
+        draggable="true"
+        @dragend="dragend"
+        v-bind="item"
+      >
         <template v-for="(field, subIndex) in item.sub" :key="subIndex">
           <el-descriptions-item>
             <template #label>
@@ -19,14 +45,20 @@ import {
   defineComponent,
   getCurrentInstance,
   onMounted,
+  reactive,
   toRaw,
+  toRefs,
 } from "vue";
 import { useRoute } from "vue-router";
 import { ElLoading } from "element-plus";
 export default defineComponent({
+  emits: ["finish"],
   props: ["config", "params", "apiUrl", "loading"],
   setup(props, { emit }) {
     const route = useRoute();
+    const state = reactive({
+      targetObj: [],
+    });
     const { proxy } = getCurrentInstance();
     onMounted(() => {
       getDetails();
@@ -41,14 +73,41 @@ export default defineComponent({
         }
       });
     };
+    const dragstart = (event, item, index) => {
+      console.log("dragstart拖拽开始事件，绑定于被拖拽元素上", event, index);
+      event.dataTransfer.setData("item", JSON.stringify(item));
+    };
+    const drop = (event) => {
+      //这里面去重操作。
+      state.targetObj.push(JSON.parse(event.dataTransfer.getData("item")));
+    };
+    const dragend = (event) => {
+      console.log("dragend", event);
+      event.dataTransfer.clearData();
+    };
     return {
+      ...toRefs(state),
       getDetails,
+      dragstart,
+      drop,
+      dragend,
     };
   },
 });
 </script>
 <style lang="scss" scoped>
 .page-content-details {
+  .descriptions-margin {
+    background-color: white;
+    margin: 15px 0;
+    padding: 15px;
+    border-radius: 4px;
+  }
+}
+.page-content-details-target {
+  width: 100%;
+  height: 700px;
+  background-color: white;
   .descriptions-margin {
     background-color: white;
     margin: 15px 0;
